ee4bed6390d01635f0d0f98b8a4b131feecd9247
ashishjnaik <ashish.j.naik@gmail.com>
2026-04-01 05:15:32 +0530

    Update app.py
    UPDATE /login block: code_challenge_method=None  # Disable PKCE — we're using confidential client

--- app.py
+++ app.py
@@ -1844,7 +1844,8 @@ def login():
     auth_url, state = flow.authorization_url(
         prompt='consent', 
         access_type='offline', 
-        state="JAGDISHWARAM_UAT"
+        state="JAGDISHWARAM_UAT",
+        code_challenge_method=None
     )
     
     if auth_url.startswith('http://'):
